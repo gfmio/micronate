c893483b99e2743b96722a0aff2f2cff544d1d8c
Frédérique Mittelstaedt <frederique@frederique-mittelstaedt.com>
2014-10-11 05:08:02 +0200

    Inserted PW

--- helpers/DB.php
+++ helpers/DB.php
@@ -5,7 +5,7 @@ class DB {
     public static $pdo = null;
 
     public static function init() {
-      self::$pdo = new PDO('mysql:host=;dbname=micronate_db', 'micronate_user', '');
+      self::$pdo = new PDO('mysql:host=;dbname=micronate_db', 'micronate_user', '^?8k43&394i?L.FXGwDhAbiRsEmH.JBhqD@[xG#N3H7q8xqN/7');
     }
 }
 
