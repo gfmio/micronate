3d12abfde7fe3266b10f3f7851a6e50c62cbacc3
Antonio Marino <antonio.marino@cern.ch>
2014-10-11 16:09:11 +0200

    Added developer api methods

--- helpers/routing/api.php
+++ helpers/routing/api.php
@@ -8,3 +8,31 @@ $app->get('/api','APIrequest',function() use($app){
 		'msg' => 'welcome to my API!',
 	));
 });
+
+$app->post('/api/authenticate_user', function() use($app){
+
+	$params = $app->request->post();
+
+	$api_key = $params['api_key'];
+
+	$appId = Application::findId($api_key);
+	if ($appId == 0) {
+		$app->render(401, array(
+				'msg' => 'Api key not valid',
+		));
+	}
+
+	$application = new Application($appId);
+
+	$userId = User::verifyCredentials($params['email'], $params['password']
+	if ($userId == 0) {
+    	$app->render(401, array(
+					'msg' => 'User credentials not valid',
+			));
+	}
+
+	$user = new User($userId);
+	$user_token = $user->authorizeApp($application);
+
+
+});
