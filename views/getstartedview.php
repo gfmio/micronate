9ea3e6f8e364422aaf032ace508f48ad4cc713ea
Frédérique Mittelstaedt <frederique@frederique-mittelstaedt.com>
2014-10-12 06:11:34 +0200

    Updated get started form

--- views/getstartedview.php
+++ views/getstartedview.php
@@ -4,39 +4,35 @@ class GetStartedView extends SimpleView {
 	function _render() {
 		?>
 		<?php $headerView = new HeaderView(); echo $headerView->render(); ?>
-		<div class="frame full-width full-height bg-darker-grey parallax hpadding-small">
-			<div class="vcenter">
-				<div class="center text-white" id="title-content">
-					<h1 class="text-center text-xxhuge container center vmargin-medium">
-						<span class="text-orange">m</span><span class="text-turquois">N</span>
-					</h1>
-					
-					<div class="left text-small hpadding-large hmargin-large">
-					<h3> Sign Up </h3>
-					<form action="signup" method="POST" class="credentials-box text-darker-grey">
-						<input type="text" name="email" placeholder="Email" class="field"><br>
-						<input type="password" name="pwd" placeholder="Password" class="field"><br>
-						<input type="password" name="pwd" placeholder="Re-type Password" class="field"><br>
-						<input class="bg-orange button text-white center" type="submit" value="Register">
-					</form>
-				</div>
-				<div class="right text-small hpadding-large hmargin-large">
-					<h3> Login </h3>
-					<form action="login" method="POST" class='credentials-box text-darker-grey'>
-						<input type="text" name="username" class="field" placeholder="Username"><br>
-						<input type="password" name="pwd" class="field" placeholder="Password"><br>
-						<input class='bg-orange button text-white center' type="submit" value="Login">
-					</form>
-				</div>
+		<div class="frame full-width full-height bg-darker-grey landing parallax hpadding-small">
+				<h1 class="text-center text-white vmargin-large">
+					Get started
+				</h1>
+			<div class="text-center center">
+				<div class="center text-center clear compact">
+					<div style="display: inline-block;" class="left hpadding-large">
+						<h3 class="text-white"> Sign Up </h3>
+						<form action="signup" method="POST" class="credentials-box text-darker-grey">
+							<input requied type="text" name="first_name" placeholder="First name" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input requied type="text" name="last_name" placeholder="Last name" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input requied type="text" name="username" placeholder="Username" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input requied type="email" name="email" placeholder="Email" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input requied type="password" name="pwd" placeholder="Password" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input requied type="password" name="pwd" placeholder="Re-type Password" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input class="bg-orange button text-white center hpadding-small vpadding-small" type="submit" value="Register" style="margin: 0; border: 0; width: 100%;">
+						</form>
 					</div>
-				
-			</div>
 
-			<footer class="bottom frame vpadding-small" style="left: 0;">
-				<p class="text-left text-white text-center container center">
-					Copyright &copy; by Micronate 2014. <a target="_blank" href="https://twitter.com/MicronateOrg" class="text-orange">@MicronateOrg</a>. Crafted with love at <a target="_blank" href="http://www.hackzurich.com/14">HackZurich 2014</a>. Image © Pro Juventute. 
-				</p>
-			</footer>
+					<div style="display: inline-block;" class="right hpadding-large">
+						<h3 class="text-white"> Sign in </h3>
+						<form action="login" method="POST" class='credentials-box text-darker-grey'>
+							<input required type="email" name="email" class="field hpadding-small vpadding-small" placeholder="E-mail" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input required type="password" name="pwd" class="field hpadding-small vpadding-small" placeholder="Password" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
+							<input class='bg-orange button text-white center hpadding-small vpadding-small' type="submit" value="Login" style="margin: 0; border: 0; width: 100%;">
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 		<?php
 	}
