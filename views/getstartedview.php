0bd4f349cefdb4f61211014ca813d72fd82945d3
Frédérique Mittelstaedt <frederique@frederique-mittelstaedt.com>
2014-10-12 06:25:29 +0200

    Small Bugfix for design issues at Get Started

--- views/getstartedview.php
+++ views/getstartedview.php
@@ -4,15 +4,15 @@ class GetStartedView extends SimpleView {
 	function _render() {
 		?>
 		<?php $headerView = new HeaderView(); echo $headerView->render(); ?>
-		<div class="frame full-width full-height bg-darker-grey landing parallax hpadding-small">
+		<div class="frame full-width bg-darker-grey landing parallax hpadding-small" style="background-size: cover;">
 				<h1 class="text-center text-white vmargin-large">
 					Get started
 				</h1>
-			<div class="text-center center">
+			<div class="text-center center clear">
 				<div class="center text-center clear compact">
-					<div style="display: inline-block;" class="left hpadding-large">
+					<div style="display: inline-block; width: 480px; max-width: 50%;" class="left hpadding-large">
 						<h3 class="text-white"> Sign Up </h3>
-						<form action="signup" method="POST" class="credentials-box text-darker-grey">
+						<form action="signup" method="POST" class="text-darker-grey">
 							<input requied type="text" name="first_name" placeholder="First name" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
 							<input requied type="text" name="last_name" placeholder="Last name" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
 							<input requied type="text" name="username" placeholder="Username" class="field hpadding-small vpadding-small" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
@@ -23,9 +23,9 @@ class GetStartedView extends SimpleView {
 						</form>
 					</div>
 
-					<div style="display: inline-block;" class="right hpadding-large">
+					<div style="display: inline-block; width: 480px; max-width: 50%;" class="right hpadding-large">
 						<h3 class="text-white"> Sign in </h3>
-						<form action="login" method="POST" class='credentials-box text-darker-grey'>
+						<form action="login" method="POST" class='text-darker-grey'>
 							<input required type="email" name="email" class="field hpadding-small vpadding-small" placeholder="E-mail" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
 							<input required type="password" name="password" class="field hpadding-small vpadding-small" placeholder="Password" style="margin: 0; border: 0; border-bottom: #DDDDDD solid 1px;"><br>
 							<input class='bg-orange button text-white center hpadding-small vpadding-small' type="submit" value="Login" style="margin: 0; border: 0; width: 100%;">
@@ -33,12 +33,13 @@ class GetStartedView extends SimpleView {
 					</div>
 				</div>
 			</div>
+
+			<footer class="vpadding-small" style="left: 0;">
+				<p class="text-left text-white text-center container center">
+					Copyright &copy; by Micronate 2014. <a target="_blank" href="https://twitter.com/MicronateOrg" class="text-orange">@MicronateOrg</a>. Crafted with love at <a target="_blank" href="http://www.hackzurich.com/14">HackZurich 2014</a>. Image © Pro Juventute. 
+				</p>
+			</footer>
 		</div>
-		<footer class="bottom frame vpadding-small" style="left: 0;">
-			<p class="text-left text-white text-center container center">
-				Copyright &copy; by Micronate 2014. <a target="_blank" href="https://twitter.com/MicronateOrg" class="text-orange">@MicronateOrg</a>. Crafted with love at <a target="_blank" href="http://www.hackzurich.com/14">HackZurich 2014</a>. Image © Pro Juventute. 
-			</p>
-		</footer>
 		<?php
 	}
 }
