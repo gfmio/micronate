738fd7a610e646beffdcff41a3fc287c8f6b7c16
marsty5 <marsty5@gmail.com>
2014-10-12 06:07:54 +0200

    Add pie charts for campaigns

--- views/campaignview.php
+++ views/campaignview.php
@@ -5,26 +5,37 @@ class CampaignView extends SimpleView {
 		?>
 <h1> {data.title}
 <h3> {data.creator} </h3>
-<img src={data.image} alt="cause1" style="width:228px;height:228px">
-<canvas id="doughNutChartLoc" width="400" height="400"></canvas>
-<script>
-        var pieChartData = [
-        {
-            value: 50,
-            color: "lightblue"
-        },
-        {
-            value: 10,
-            color: "red"
-        },
-        {
-            value: 40,
-            color: "green"
-        }
-        ]
-        var myDoughnut = new Chart(document.getElementById("doughNutChartLoc").getContext("2d")).Doughnut(DoughNutChartData);
-</script>
 <p><b>Location</b>: {data.location}</p>
+<img src={data.image} alt="cause1" style="width:228px;height:228px">
+<div class="full-width hpadding-small">
+	<div class="discover-container center">
+	<?php if (empty($this->data["donations"])): ?>
+		<p class="text-center">There are no donations for this campaign yet. :( </p>
+	<?php else: ?>
+	    <canvas id="doughNutChartLoc" width="400" height="400"></canvas>
+	    <script>
+	    <?php var $colors = array("lightblue", "lightgreen", "lightgrey"); ?>
+	    var pieChartData = [
+	        <?php var $i = 0; ?>
+	        <?php foreach ($this->data["donations"] as $c): ?>
+	        {
+	        	value: <?php echo $c["value"]; ?>,
+	        	color: <?php echo $colors[i]; ?>
+	        }
+	        <?php $i++; ?>
+	        <?php if ($i != count($this->data["donations"])): ?>
+	        ,
+	        <?php else: ?>
+	        ]
+	        <?php endif; ?>
+
+	        <?php endforeach; ?>
+	        var myDoughnut = new Chart(document.getElementById("doughNutChartLoc").getContext("2d")).Doughnut(DoughNutChartData);
+	    </script>	
+	<?php endif; ?>
+	</div>
+</div>
+
 <p><b>Goal</b>: {data.goal}       <b>Reached</b>: {data.reached}     <b>Deadline</b>: {data.date} </p>
 
 <h3> Short Description</h3>
